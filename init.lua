6b2dc90d27e3b122818ee69acf3703396c0bb009
manas140 <manasbhosale.14@gmail.com>
2023-04-07 19:08:33 +0000

    fixes

--- init.lua
+++ init.lua
@@ -22,7 +22,7 @@ local closeChars = {
 }
 
 local function toSkip(chr)
-  return chr:match(skipChars) ~= nil
+  return chr:find(skipChars) ~= nil
 end
 
 command.add(predicate, {
@@ -35,7 +35,7 @@ command.add(predicate, {
 
     if toSkip(next) and (doc:get_text(l, 1, l, c):find('^%s+$') == nil and c > 1) then
       doc:set_selection(l, c+1)
-    elseif toSkip(prev) then
+    elseif toSkip(prev) and c > 1 then
       local text = line:sub(c, -1)
       local new_pos = nil
 
@@ -46,7 +46,7 @@ command.add(predicate, {
       if similar ~= nil then -- cursor to next similar char
         new_pos = similar - 1
       elseif close ~= nil then -- cursor to closing char or skip current char
-        new_pos = (text:find(close)-1 or skip)
+        new_pos = (text:find(close) or skip) - 1
       else
         new_pos = skip -- cursor skips current char or on line end
       end
